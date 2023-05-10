36b92e4f473fc64a41c07361203c3f18dd65b36d
joe weber <jweber4213@gmail.com>
2023-05-10 15:18:45 +0900

    solved

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  Rails.application.routes.draw do
+    resources :restaurants do
+      resources :reviews, only: [:new, :index, :create]
+    end
+    resources :reviews, only: [:destroy]
+  end
+
 end
